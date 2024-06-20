0902b841babe03781dd635743cbf4e7ef0bb6a72
CrazyTechieAutomating <60341560+crazytechie1990@users.noreply.github.com>
2024-06-20 11:11:59 +0100

    [BAU] take out azapi as no aca needed

--- versions.tf
+++ versions.tf
@@ -5,20 +5,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.72.0"
     }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 1.12.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-  }
 
   cloud {}
 }
-
-provider "random" {}
-
 
 provider "azurerm" {
   features {}

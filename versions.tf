b2e0480171c824e94484181473497abdab589689
CrazyTechieAutomating <60341560+crazytechie1990@users.noreply.github.com>
2024-06-20 11:17:10 +0100

    [BAU] test cloud block again

--- versions.tf
+++ versions.tf
@@ -1,14 +1,24 @@
 terraform {
-  required_version = "~> 1.7.3"
+  required_version = "1.7.3"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.72.0"
+      version = "~> 3.90.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
 
-  cloud {} 
- }
+  cloud {}
 }
+
+provider "random" {}
+
 
 provider "azurerm" {
   features {}
